b5597586a04c2a26ffcc8006948d1ee33f88a842
Romeo Namunde <romeomureithi@gmail.com>
2020-11-09 18:50:23 +0300

    Added from repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# A Random Quote Generator
+This is a simple random quote generator for some of the best legendary icons of all time
---
+++ load.js
@@ -0,0 +1,25 @@
+window.onload = init;  // this loads text everytime windows is refreshed or loaded
+
+function init() {
+    generateQuote()
+}
+
+function generateQuote(){
+    let quoteSize = quoteBank.length;
+    let randomIndex = Math.floor(Math.random() * quoteSize);
+    let randomQuoteData = quoteBank[randomIndex];
+
+    let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amptext=";
+    
+    // Adding the quote text
+    var quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
+    twitterLink += quoteInApiFormat;
+    
+    // Adding the author
+    let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20");
+    twitterLink += " - " + authorInApiFormat;
+
+    document.getElementById("tweet-quote").href = twitterLink;
+    document.getElementById("text").innerText = randomQuoteData.quote;
+    document.getElementById("author").innerText = randomQuoteData.author;
+}
